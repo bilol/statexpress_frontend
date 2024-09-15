291448f8cc03450fc7fd1856eda7217042d9cf25
Your Name <your.email@example.com>
2024-09-16 05:02:04 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './statexpress_frontend/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
