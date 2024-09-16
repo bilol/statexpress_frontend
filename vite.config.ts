5217f7c4edd77e2fa3ba87055883f67e8597c7f9
Your Name <your.email@example.com>
2024-09-16 11:26:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://stat-express-backend.vercel.app',  // Backend server for local development
+        target: 'https://stat-express-backend.vercel.app',  // Backend server for local development
         changeOrigin: true,
       },
     },
